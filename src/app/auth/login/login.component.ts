46bb2c0dc87a322364197973e609e0532bdb6183
palak-chainani <palak.chainani.st@gmail.com>
2025-08-02 19:59:13 +0530

    second commit

--- src/app/auth/login/login.component.ts
+++ src/app/auth/login/login.component.ts
@@ -1,31 +1,31 @@
-import { Component } from '@angular/core';
-import { FormGroup, FormControl, Validators } from '@angular/forms';
+import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html',
   styleUrls: ['./login.component.css']
 })
-export class LoginComponent {
+export class LoginComponent implements OnInit {
   loginForm!: FormGroup;
-  constructor() { }
+
+  constructor(private fb: FormBuilder) {}
 
   ngOnInit(): void {
-    // Initialize the FormGroup with FormControls and validators
-    this.loginForm = new FormGroup({
-      email: new FormControl('', [Validators.required, Validators.email]),
-      password: new FormControl('', [Validators.required, Validators.minLength(6)])
+    this.loginForm = this.fb.group({
+      email: ['', [Validators.required, Validators.email]],
+      password: ['', [Validators.required, Validators.minLength(6)]]
     });
   }
+
   onSubmit(): void {
     if (this.loginForm.valid) {
-      console.log('Login form submitted successfully!');
       console.log(this.loginForm.value);
     } else {
-      console.log('Form is invalid. Please check the fields.');
       this.loginForm.markAllAsTouched();
     }
   }
+
   get email() {
     return this.loginForm.get('email');
   }
@@ -33,6 +33,4 @@ export class LoginComponent {
   get password() {
     return this.loginForm.get('password');
   }
-
 }
-
